0c4dd46528f55cdd166fb664eba0c7b6e7c0d2cf
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-01-08 23:57:57 -0300

    finish Review.jsx && ReviewCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
+import Review from "./components/Review";
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
             <About />
             <Skill />
             <Work />
+            <Review />
         </main>
         </>
     )
